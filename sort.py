33f1c9f5639b80109768c0b9ef0c3b0ed07f121b
Alfa Venegas <owner@MacBook-Pro-7.local>
2019-01-13 01:07:47 -0800

    selection sort

--- sort.py
+++ sort.py
@@ -1,7 +1,17 @@
 import random
 
 numbers = []
+
 for i in range(10):
 	numbers.append(random.randint(0,100))
 
-print(numbers)
+print('Desordenados',numbers)
+
+for i in range(0,10):
+	for j in range(i+1,10):
+		if numbers[i] > numbers[j]:
+			temp = numbers[i]
+			numbers[i] = numbers[j]
+			numbers[j] = temp
+
+print('Ordenados   ', numbers)
